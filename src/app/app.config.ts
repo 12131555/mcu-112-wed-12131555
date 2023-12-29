da109d4090b5faf13ac6b67b6b7b65a8914ac8eb
12131555 <12131555@me.mcu.edu.tw>
2023-12-29 18:06:06 +0800

    refactor(todo-form): 設定路由繫結至輸入性屬性

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 import { TaskService } from './services/task.service';
 import { TaskRemoteService } from './services/task-remote.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     { provide: TaskService, useClass: TaskRemoteService },
   ],
 };
