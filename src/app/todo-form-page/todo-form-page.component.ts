7b59a687921d39059df3508b65b00fc9c95c8767
12131555 <12131555@me.mcu.edu.tw>
2023-12-29 18:00:55 +0800

    feat(todo-form): 待辦事項預載資料實作

--- src/app/todo-form-page/todo-form-page.component.ts
+++ src/app/todo-form-page/todo-form-page.component.ts
@@ -30,16 +30,17 @@ id?: number;
     this.route.paramMap
       .pipe(
         filter((paramMap) => paramMap.has('id')),
-        map((paramMap) => +paramMap.get('id')!),
-        tap((id) => (this.id = id)),
-        switchMap((id) => this.taskService.getById(id))
+        map((paramMap) => +paramMap.get('id')!)
       )
-      .subscribe((formData) => (this.formData = formData));
+      .subscribe((id) => (this.id = id));
 
       this.route.data
-      .pipe(map(({ title }) => title))
-      .subscribe((title) => (this.title = title));
-    }
+      .pipe(
+        tap(({ title }) => (this.title = title)),
+        map(({ formData }) => formData)
+      )
+      .subscribe((formData) => (this.formData = formData));
+  }
 
   onSave(task: Todo): void {
     let action$: Observable<Todo>;
